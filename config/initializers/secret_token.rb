e92947482bae5ad73064b9eb0957bda4efe92902
John Moons <j.moons@realdigitalmedia.com>
2013-04-30 13:35:19 -0400

    Initial Commit - app creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivityTracker::Application.config.secret_token = '739fea67d241e709395a6fde63dc8fa20b7194c884bfc02e40a1b5e588fa877b5ebab4c76e6db0781afddd5ca64a40d25ff11499469d745876485bac3672ff90'
